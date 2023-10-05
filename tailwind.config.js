76041afafb28badb01472b6f39d6bbec69017a9e
Luca Di Marco <lucanahtsu@gmail.com>
2023-10-05 05:36:51 -0300

    Add tailwindcss to the project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    colors: {
+      'brightorange': '#e38826',
+      'darkcyan': '#006970',
+      'verydarkcyan': '#004241',
+      'verylightgray': '#f2f2f2'
+    },
+    fontFamily: {
+      'lexenddeca': ['Lexend Deca', 'sans-serif'],
+      'bigshouldersdisplay': ['Big Shoulders Display', 'cursive']
+    },
+    extend: {
+     
+    },
+  },
+  plugins: [],
+}
+
